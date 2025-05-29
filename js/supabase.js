08c2b24d51505b82adeec6f3c2382f107336d3b6
David Prevosto <david_zeppelin@hotmail.com>
2025-05-29 20:50:51 -0300

    Inicializar proyecto - subir archivo js

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+// Configura Supabase
+const supabaseUrl = 'https://ewaqfqtlbknrzzxeamuv.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3YXFmcXRsYmtucnp6eGVhbXV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzODkzODAsImV4cCI6MjA2Mzk2NTM4MH0.VRVStkqzAJhADyAXxFs_IO9JyKU3bTlusnWXzw_1fOU';
+
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
